c88ebaad3fa6b73e3a21dbf5177102483b5638cc
yashgupta31 <yashsntoshgupta2019@gmail.com>
2024-02-06 19:28:42 +0530

    added code from location 1.

---
+++ array.js
@@ -0,0 +1,18 @@
+let arr=["yash", "aman", "mihir", "pushkar"];
+
+//access element by index
+console.log(arr[2])
+
+//access last element
+console.log(arr[arr.length-1])
+
+//update element by index
+arr[0]= "rohit"
+
+//add element at last
+arr.push("mayur")
+
+
+//remove last element
+arr.pop()
+console.log(arr)
---
+++ object.js
@@ -0,0 +1,15 @@
+let obj={
+    name: "Yash",
+    age: 22,
+    gender: "male"
+}
+
+//add item in object
+obj["result"]="pass"
+
+//remove item
+delete obj["age"]
+
+//update item
+obj["name"]="yash gupta"
+console.log(obj)
